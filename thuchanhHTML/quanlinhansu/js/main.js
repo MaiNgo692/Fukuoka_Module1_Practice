72f53d158471ec4e68283332b87aec4a1ef48497
MaiNgo692 <NgoThimai@outlook.com>
2023-11-18 14:21:05 +0900

    add modify

--- thuchanhHTML/quanlinhansu/js/main.js
+++ thuchanhHTML/quanlinhansu/js/main.js
@@ -10,20 +10,27 @@ console.log("japanlevel", japanlevel.value);
 let marks = document.getElementById("marks");
 let btnSubmit = document.getElementById("submit");
 let checkForm = true;
-function uuId() {
-    return Math.floor(Math.random()*11234455667);
-}
+
 function checkSex() {
     if (document.getElementById('male').checked) {
-        sex = "male";
+        sex = document.getElementById('male').value;
         document.getElementById(`alert-sex`).classList.add("none-display");
     } else if (document.getElementById('female').checked) {
-        sex = "female";
+        sex = document.getElementById('female').value;
         document.getElementById(`alert-sex`).classList.add("none-display");
     } else {
         checkForm = false;
         document.getElementById(`alert-sex`).innerHTML = "「性別」を選んで。";
-        document.getElementById(`alert-sex`).classList.remove("none-display")
+        document.getElementById(`alert-sex`).classList.remove("none-display");
+    }
+}
+function checkJapanLevel() {
+    if (japanlevel.value == "") {
+        checkForm = false;
+        document.getElementById(`alert-japanlevel`).innerHTML = "「日本語能力級」を選んで。";
+        document.getElementById(`alert-japanlevel`).classList.remove("none-display");
+    } else {
+        document.getElementById(`alert-japanlevel`).classList.add("none-display");
     }
 }
 function validateInput() {
@@ -34,6 +41,7 @@ function validateInput() {
     validateRequired(marks);
     checkEmail(mail);
     checkSex();
+    checkJapanLevel();
     checkTel(telnumber);
     checkMarks(marks);
     if (checkForm) {
@@ -43,7 +51,6 @@ function validateInput() {
 function addNewUser() {
     let userList = JSON.parse(localStorage.getItem("userList")) || [];
     user = {
-        id:uuId(),
         fullname: fullname.value,
         sex: sex,
         group: group.value,
@@ -54,12 +61,13 @@ function addNewUser() {
     }
     userList.push(user);
     localStorage.setItem("userList", JSON.stringify(userList));
-    // showUserList();
-    document.getElementById("fullname").value="";
-    document.getElementById("group").value="";
-    document.getElementById("mail").value="";
-    document.getElementById("telnumber").value="";
-    document.getElementById("marks").value="";
+    showUserList();
+    document.getElementById("fullname").value = "";
+    document.getElementById("group").value = "";
+    document.getElementById("mail").value = "";
+    document.getElementById("japanlevel").value = "";
+    document.getElementById("telnumber").value = "";
+    document.getElementById("marks").value = "";
 }
 function checkEmail(mail) {
     if (mail.value != "") {
@@ -98,40 +106,36 @@ function validateRequired(elementName) {
     let elId = elementName.id;
     let inputValue = elementName.value;
     let lables = document.getElementsByTagName('label');
-    if (inputValue == "") {
-        for (let i = 0; i < lables.length; i++) {
-            if (lables[i].htmlFor == elId) {
+    for (let i = 0; i < lables.length; i++) {
+        if (lables[i].htmlFor == elId) {
+            if (inputValue == "") {
                 checkForm = false;
                 document.getElementById(`alert-${elId}`).innerHTML = `「${lables[i].textContent}」を入力して。`;
                 document.getElementById(`alert-${elId}`).classList.remove("none-display");
-                break;
-            }
-        }
-    }else{
-        for (let i = 0; i < lables.length; i++) {
-            if (lables[i].htmlFor == elId) {
+            } else {
                 document.getElementById(`alert-${elId}`).classList.add("none-display");
-                break;
             }
+            break;
         }
     }
 }
 function showUserList() {
     let userList = JSON.parse(localStorage.getItem("userList")) || [];
     let text = "";
+
     for (let i = 0; i < userList.length; i++) {
+        let getSex = userList[i].sex==1?'男':'女'
         text += `
         <tr>
             <td>${i + 1}</td>
             <td>${userList[i].fullname}</td>
-            <td>${userList[i].sex}</td>
+            <td>${getSex}</td>
             <td>${userList[i].group}</td>
             <td>${userList[i].mail}</td>
             <td>${userList[i].telnumber}</td>
             <td>${userList[i].japanlevel}</td>
             <td>${userList[i].marks}</td>
-            <td><a onclick="deleteUser(${userList[i].id
-            })">削除</a></td>
+            <td><a onclick="deleteUser(${i})">削除</a></td>
         </tr>
         `;
     }
@@ -140,14 +144,9 @@ function showUserList() {
 showUserList();
 function deleteUser(userId) {
     let userList = JSON.parse(localStorage.getItem("userList")) || [];
-    for (let i = 0; i < userList.length; i++) {
-        if(userList[i].id == userId){
-            if(confirm("削除しますか。") == true){
-                userList.splice(i,1);
-                localStorage.setItem("userList", JSON.stringify(userList));
-            }
-        }
-        
+    if (confirm("削除しますか。") == true) {
+        userList.splice(userId, 1);
+        localStorage.setItem("userList", JSON.stringify(userList));
     }
     showUserList();
 }
